5964e0cdae7a453c62fcbb4166faded4b110c9f8
PragadeeshRaj <70968487+PragadeeshRaj@users.noreply.github.com>
2020-10-23 10:06:53 -0700

    Delete sqrt.py

--- sqrt.py
+++
@@ -1,8 +0,0 @@
-#Navodit Maheshwari
-#Square root Calculator
-
-import math  #math module
-def sqrt():
-  print("Enter a number: ")
-  a = int(input())  #User inputs value
-  print(math.sqrt(a)) #prints square root
