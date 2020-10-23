88e7053c44716567506339b24529707090031b22
PragadeeshRaj <70968487+PragadeeshRaj@users.noreply.github.com>
2020-10-23 10:06:27 -0700

    Delete Subtraction.py

--- Subtraction.py
+++
@@ -1,6 +0,0 @@
-#Brandon Truong
-def Subtraction():#code takes 2 values and subtrats 1st from 2nd
-  print("Input 2 values and it will subtract them")
-  x = int(input("enter a number:"))
-  y = int(input("enter a 2nd number:"))
-  print("answer =", x - y)
