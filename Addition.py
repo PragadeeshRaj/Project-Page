2a03ed5c555262e284e84edc18c6bac8aae0550c
PragadeeshRaj <70968487+PragadeeshRaj@users.noreply.github.com>
2020-10-23 10:05:28 -0700

    Delete Addition.py

--- Addition.py
+++
@@ -1,6 +0,0 @@
-#Pragadeesh Raj
-def Sum(): #user inputs two numbers
-  num1 = int(input("Enter a Number: "))
-  num2 = int(input("Enter a Number: "))
-  print("Sum = ", num1 + num2)
-  #prints the sum of the two inputs
