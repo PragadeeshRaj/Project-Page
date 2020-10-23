8ece56a269f85fb8adef7727df67e0849ee76478
PragadeeshRaj <70968487+PragadeeshRaj@users.noreply.github.com>
2020-10-23 10:05:44 -0700

    Delete Exponent.py

--- Exponent.py
+++
@@ -1,8 +0,0 @@
-#Navodit Maheshwari
-def Exponent():#program puts first value to power of other
-
-  print("What is the base value?")
-  b = int(input())
-  print("What is the exponent?")
-  e = int(input())
-  print(b ** e) #The syntax for putting a value to the power of another is "**"
