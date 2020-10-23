60dcb99c58757805d9e47c4c6aded42ba5a82732
PragadeeshRaj <70968487+PragadeeshRaj@users.noreply.github.com>
2020-10-23 10:05:36 -0700

    Delete Division.py

--- Division.py
+++
@@ -1,6 +0,0 @@
-#Ali Saad
-def  Divide(): #This program divides two user inputted numbers
-  number1 = float(input("Enter your first number"))
-  number2 = float(input("Enter your second number"))
-  result = str(number1 / number2) #this piece divides the first number by the second number
-  print ("Your answer is " + result)
