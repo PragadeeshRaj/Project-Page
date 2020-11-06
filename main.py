b2cda6755784f479bc05e84369917e0f55d38509
Pragadeesh Raj <pragadeesh3000@gmail.com>
2020-11-06 15:35:17 -0800

    Worked on Dropdown and Individual Videos

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import projects     # imports projects data from projects.py
+import projects  # imports projects data from projects.py
 
-from flask import Flask, render_template    # imports the render template
+from flask import Flask, render_template  # imports the render template
 
 app = Flask(__name__)
 
 
-@app.route('/')     # decorates the url with a route
+@app.route('/')  # decorates the url with a route
 def home_route():
     return render_template("home.html", projects=projects.setup())
 
@@ -21,13 +21,35 @@ def calculator_route():
     return render_template("calculator.html", projects=projects.setup())
 
 
-@app.route('/videos/')
-def video_route():
-    return render_template("videos.html", projects=projects.setup())
+@app.route('/ayman/')
+def ayman_route():
+    return render_template("ayman.html", projects=projects.setup())
+
+
+@app.route('/pragadeesh/')
+def pragadeesh_route():
+    return render_template("pragadeesh.html", projects=projects.setup())
+
+
+@app.route('/brandon/')
+def brandon_route():
+    return render_template("brandon.html", projects=projects.setup())
+
+
+@app.route('/ali/')
+def ali_route():
+    return render_template("ali.html", projects=projects.setup())
+
+
+@app.route('/navodit/')
+def navodit_route():
+    return render_template("navodit.html", projects=projects.setup())
+
 
 @app.route('/journal/')
-def Journal_route():
+def journal_route():
     return render_template("journal.html", projects=projects.setup())
+
 
 if __name__ == "__main__":
     app.run(port='5000', host='127.0.0.1')
