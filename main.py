0f7d0ebee4038e020ee2066d43d552547fcbdab3
Pragadeesh Raj <pragadeesh3000@gmail.com>
2020-11-18 16:21:09 -0800

    Template updates

--- main.py
+++ main.py
@@ -52,5 +52,5 @@ def journal_route():
 
 
 if __name__ == "__main__":
-    app.run(port='8080', host='66.172.116.112')
+    app.run(port='5000', host='127.0.0.1')
     # runs at IntelliJ port with LAN host
